0989adab550cae5a63bb4d4f90a00d65e9c2857a
arybary <l3fljl5ldydy@gmail.com>
2023-08-19 11:39:07 +0300

    add layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [{ path: '', redirectTo: 'github_list', pathMatch: 'full' }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,11 +6,18 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment.development';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
+    RouterModule,
     StoreRouterConnectingModule.forRoot(),
     StoreModule.forRoot(appRedusers, { metaReducers }),
     EffectsModule.forRoot(),
